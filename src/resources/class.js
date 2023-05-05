e5f00414c0abea0ae460180e17ce8aff3c4387cd
FrancoLelli <francogastonlelli@gmail.com>
2023-05-05 18:21:41 -0300

    MR-08: Update Get Class endpoint

--- src/resources/class.js
+++ src/resources/class.js
@@ -1,19 +1,19 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-const classJSON = require("../data/class.json");
+const classJSON = require('../data/class.json');
 
 const classRoute = Router();
 
-classRoute.get("/:id", (req, res) => {
- const { id } = req.params;
- const classFilter = classJSON.find(
-  (classElement) => classElement.id.toString() === id
- );
- if (!classFilter) {
-  res.send("Not exists this ID");
- } else {
-  res.send(classFilter);
- }
+classRoute.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const classFilter = classJSON.find(
+    (classElement) => classElement.id.toString() === id,
+  );
+  if (!classFilter) {
+    res.send('Not exists this ID');
+  } else {
+    res.send(classFilter);
+  }
 });
 
 export default classRoute;
