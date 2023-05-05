a3edd3fa922eb09e471b62c128746e5f0270c74f
Milagros <cerromilagros1@gmail.com>
2023-05-05 16:33:58 -0300

    MR-08:create class

--- src/resources/class.js
+++ src/resources/class.js
@@ -1,20 +1,23 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import fs from "fs";
+import fs from 'fs';
 
 const classRoute = Router();
 
-const classJSON = require("../data/class.json");
-const path = "./src/data/class.json";
+const classJSON = require('../data/class.json');
 
-classRoute.post("/", (req, res) => {
+const path = './src/data/class.json';
+
+classRoute.post('/', (req, res) => {
   const { trainer, activity, duration } = req.body;
   const idValue = classJSON.length + 1;
-  const newClass = {id: idValue, trainer, activity, duration };
+  const newClass = {
+    id: idValue, trainer, activity, duration,
+  };
   classJSON.push(newClass);
   fs.promises.writeFile(path, JSON.stringify(classJSON, null, 2), (err) => {
     if (err) {
-      console.log(err);
+      console.error(err);
     }
   });
   res.send(classJSON);
