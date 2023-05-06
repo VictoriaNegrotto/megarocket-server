fffba466f3bbb7ca4fb7a0e41231462527a8c232
Alejandro Schmidt <alemusho@gmail.com>
2023-05-06 17:34:25 -0300

    MR: Comments in code and status 200 fixed

--- src/resources/subscription.js
+++ src/resources/subscription.js
@@ -1,11 +1,3 @@
-// Crear un Subscription (suscripción a una clase en determinada fecha)
-// Editar un Subscription
-// Obtener un Subscription
-// Eliminar un Subscription
-// Obtener la lista de Subscription con la opción de usar filtros
-
-// const express = require('express');
-
 import { Router } from 'express';
 
 const subscriptions = require('../data/subscription.json');
@@ -13,12 +5,9 @@ const subscriptions = require('../data/subscription.json');
 const subscriptionsRouter = Router();
 
 subscriptionsRouter.get('/', (req, res) => {
-  res.status(200).json({
+  res.json({
     data: subscriptions,
   });
-  // res.send(subscriptions);
 });
-
-// module.exports = subscriptionsRouter;
 
 export default subscriptionsRouter;
