ea8629591dc2d35bb9aa65ce24694d6f9e9f6892
Alejandro Schmidt <alemusho@gmail.com>
2023-05-06 16:37:32 -0300

    MR: Get data from Json file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 // use "import" to import libraries
 import express from 'express';
 import cors from 'cors';
+import subscriptionsRouter from './resources/subscription';
 
 // use "require" to import JSON files
 const admins = require('./data/admins.json');
+
+// const subscriptionsRouter = require('./resources/subscription');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/subscriptions', subscriptionsRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
