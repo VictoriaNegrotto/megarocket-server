cca43ac72e2283047b834c22120c4d05e9bba407
FrancoLelli <francogastonlelli@gmail.com>
2023-05-06 10:33:13 -0300

    MR-08: Create assign trainer endpoint

--- src/resources/class.js
+++ src/resources/class.js
@@ -1,6 +1,9 @@
 import { Router } from 'express';
+import fs from 'fs';
 
 const classJSON = require('../data/class.json');
+
+const path = './src/data/class.json';
 
 const classRoute = Router();
 
@@ -13,6 +16,25 @@ classRoute.get('/:id', (req, res) => {
   if (!classFilter) return res.send('Not exists this ID');
 
   return res.send(classFilter);
+});
+
+classRoute.patch('/trainer', (req, res) => {
+  const { id, trainer } = req.body;
+
+  const classFilter = classJSON.find(
+    (classElement) => classElement.id.toString() === id,
+  );
+
+  if (!classFilter) return res.send('Not exists this ID');
+
+  classFilter.trainer = trainer;
+
+  fs.writeFile(path, JSON.stringify(classJSON, null, 2), (err) => {
+    if (err) {
+      res.send(err);
+    }
+  });
+  return res.send('Class updated with new trainer');
 });
 
 export default classRoute;
