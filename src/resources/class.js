24ff960d768bf753acbe0ee90b815fa126d78fd9
Milagros <cerromilagros1@gmail.com>
2023-05-06 14:57:58 -0300

    MR-08: create class delete request

--- src/resources/class.js
+++ src/resources/class.js
@@ -15,12 +15,29 @@ classRoute.post('/', (req, res) => {
     id: idValue, trainer, activity, duration,
   };
   classJSON.push(newClass);
-  fs.promises.writeFile(path, JSON.stringify(classJSON, null, 2), (err) => {
+  fs.writeFile(path, JSON.stringify(classJSON, null, 2), (err) => {
     if (err) {
-      res.send(err);
+      res.send('Error! Class canot be created!');
     }
   });
-  res.send(classJSON);
+  res.send('Class created successfuly');
+});
+
+classRoute.delete('/:id', (req, res) => {
+  const classId = parseInt(req.params.id, 10);
+  const filteredClass = classJSON.filter((classObj) => classObj.id === classId);
+  if (filteredClass.length === 0) {
+    res.send('Class not found');
+    return;
+  }
+  const filteredJSON = classJSON.filter((classObj) => classObj.id !== classId);
+  fs.writeFile(path, JSON.stringify(filteredJSON, null, 2), (err) => {
+    if (err) {
+      res.send('Error deleting class');
+    } else {
+      res.send('Class deleted!');
+    }
+  });
 });
 
 export default classRoute;
