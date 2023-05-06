947e264619a01195bf216fef32a961622f8b8bfe
TomasRomanoo <romano.tomas@outlook.com.ar>
2023-05-06 17:13:52 -0300

    MR-08: create get-delete functionality

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 // use "import" to import libraries
 import express from 'express';
 import cors from 'cors';
+import activityRouter from './resources/activity';
 
 // use "require" to import JSON files
-const admins = require('./data/admins.json');
+const activities = require('./data/activity.json');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/activity', activityRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/admins', (req, res) => {
+app.get('/activity', (req, res) => {
   res.status(200).json({
-    data: admins,
+    data: activities,
   });
 });
 
