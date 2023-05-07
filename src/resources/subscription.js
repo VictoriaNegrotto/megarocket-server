a35bb081f5aeeccc6206a3a533afce31166f1cb1
Alejandro Schmidt <alemusho@gmail.com>
2023-05-07 15:21:39 -0300

    MR: delete-subscription-by-id

--- src/resources/subscription.js
+++ src/resources/subscription.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import fs from 'fs';
 
 const subscriptions = require('../data/subscription.json');
 
@@ -17,6 +18,23 @@ subscriptionsRouter.get('/getById/:id', (req, res) => {
     res.send(foundSubsc);
   } else {
     res.send('Subscription not found');
+  }
+});
+
+subscriptionsRouter.delete('/deleteById/:id', (req, res) => {
+  const subscriptionId = req.params.id;
+  const foundSubsc = subscriptions.filter((subsc) => subsc.id.toString() !== subscriptionId);
+
+  if (subscriptionId > subscriptions.length) {
+    res.send('Error! This id does not exist');
+  } else {
+    fs.writeFile('src/data/subscription.json', JSON.stringify(foundSubsc, null, 2), (err) => {
+      if (err) {
+        res.send('Error! Subscription cannot be deleted');
+      } else {
+        res.send('Subscription deleted');
+      }
+    });
   }
 });
 
