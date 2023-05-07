336ffd92f8c70fa34db1e7b9ac8ca6a12b8863b0
Juan Cruz Tilatti <jctilatti@gmail.com>
2023-05-07 16:03:21 -0300

    MR-08: Finished edit request

--- src/resources/admins.js
+++ src/resources/admins.js
@@ -1,20 +1,37 @@
 import express from 'express';
+import fs from 'fs';
 
 const admins = require('../data/admins.json');
 
 const router = express.Router();
 
-router.get('/get', (req, res) => {
+router.get('/', (req, res) => {
   res.send(admins);
 });
 
-router.get('/getById/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const adminId = req.params.id;
   const foundAdmin = admins.find((admin) => admin.id.toString() === adminId);
   if (foundAdmin) {
     res.send(foundAdmin);
   } else {
     res.send('Admin not found');
+  }
+});
+
+router.put('/:id', (req, res) => {
+  const adminId = req.params.id;
+  const editAdmin = req.body;
+  const index = admins.findIndex((admin) => admin.id.toString() === adminId);
+
+  if (index !== -1) {
+    admins[index] = { id: adminId, ...editAdmin };
+    fs.writeFile('src/data/admins.json', JSON.stringify(admins, null, 2), (err) => {
+      if (err) {
+        res.send('Error admin cannot be edited');
+      }
+    });
+    res.send('Admin edited');
   }
 });
 
