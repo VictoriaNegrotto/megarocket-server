ea07a07812dfab60193045a1aa47f9c34a5e0fbd
Juan Cruz Tilatti <jctilatti@gmail.com>
2023-05-07 00:35:21 -0300

    MR-08: Finished get, and get by ID

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import express from 'express';
 import cors from 'cors';
 import routerMembers from './resources/member';
 import classRoute from './resources/class';
+import adminRoute from './resources/admins';
 
 // use "require" to import JSON files
-const admins = require('./data/admins.json');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -13,17 +13,11 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 app.use('/members', routerMembers);
-
+app.use('/admins', adminRoute);
 app.use('/class', classRoute);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-});
-
-app.get('/admins', (req, res) => {
-  res.status(200).json({
-    data: admins,
-  });
 });
 
 app.listen(port, () => {
