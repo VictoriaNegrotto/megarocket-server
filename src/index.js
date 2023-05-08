043526f4446750566319ab8ef94d4abdf5bc030a
julianv97 <julian.vicente@radiumrocket.com>
2023-05-08 09:12:23 -0300

    MR-08: add resources to index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 // use "import" to import libraries
 import express from 'express';
 import cors from 'cors';
+import activityRouter from './resources/activity';
+import superAdminsRoute from './resources/super-admins';
+import routerMembers from './resources/member';
+import classRoute from './resources/class';
+import subscriptionsRouter from './resources/subscription';
 
 // use "require" to import JSON files
 const admins = require('./data/admins.json');
 
 const app = express();
 const port = process.env.PORT || 4000;
-// const activityRouter = require('./resources/activity');
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +19,12 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use('/activity', activityRouter);
+app.use('/super-admins', superAdminsRoute);
+app.use('/members', routerMembers);
+app.use('/class', classRoute);
+app.use('/subscriptions', subscriptionsRouter);
 
 app.get('/admins', (req, res) => {
   res.status(200).json({
