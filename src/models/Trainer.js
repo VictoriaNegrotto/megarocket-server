0accb5c84b86bcbb462b03a55a8061f07e9c0983
Milagros <cerromilagros1@gmail.com>
2023-05-12 15:14:17 -0300

    MR-06: add min and max lenght

--- src/models/Trainer.js
+++ src/models/Trainer.js
@@ -1,38 +1,53 @@
 import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
-
 const trainerSchema = new Schema({
   firstName: {
     type: String,
+    minLength: 3,
+    maxLength: 20,
     required: true,
   },
   lastName: {
     type: String,
+    minLength: 3,
+    maxLength: 20,
     required: true,
   },
   dni: {
     type: Number,
+    min: 1000000,
+    max: 99999999,
     required: true,
   },
   phone: {
     type: Number,
     required: true,
+    max: 99999999,
   },
   email: {
     type: String,
+    minLength: 5,
+    maxLength: 20,
     required: true,
+    lowercase: true,
   },
   city: {
     type: String,
+    minLength: 3,
+    maxLength: 20,
     required: true,
   },
   password: {
     type: String,
+    minLength: 3,
+    maxLength: 20,
     required: true,
   },
   salary: {
     type: Number,
+    min: 0,
+    required: true,
   },
   isActive: {
     type: Boolean,
