529c897c774ad2d0d84e99fd5b886c6fd2833fe6
Milagros <cerromilagros1@gmail.com>
2023-05-12 13:28:41 -0300

    MR-06:create the schema for trainer

---
+++ src/models/Trainer.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const trainerSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  dni: {
+    type: Number,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  salary: {
+    type: Number,
+  },
+  is_active: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default mongoose.model('Trainer', trainerSchema);
