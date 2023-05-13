0eef404f879d6db335b3796bc2386d9b25c50e7e
TomasRomanoo <romano.tomas@outlook.com.ar>
2023-05-13 18:37:24 -0300

    Joi validate done

---
+++ src/routes/activity.js
@@ -0,0 +1,14 @@
+// const express = require('express');
+
+// const activityController = require('../controllers/activity');
+
+// const validations = require('../validations/activity');
+
+// const router = express.Router();
+
+// router
+//   .get("/",activityController.getAllActivity);
+//   .get("/",activityController.getActivityById);
+//   .post("/",validations.validateCreation, activityController.createActivity);
+
+// module.exports = router
