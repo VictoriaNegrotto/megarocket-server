bc31a88b198f8c8a1f0e45f76562e9a321ee5d91
FrancoLelli <francogastonlelli@gmail.com>
2023-05-12 21:06:24 -0300

    MR-5: Fix super admin email

--- src/models/Super-admin.js
+++ src/models/Super-admin.js
@@ -5,7 +5,10 @@ const { Schema } = mongoose;
 const superAdminSchema = new Schema({
   email: {
     type: String,
+    minLength: 5,
+    maxLength: 30,
     required: true,
+    lowercase: true,
   },
   password: {
     type: String,
