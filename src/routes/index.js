7cb9d5e7dd97175c0ef50532e4a601db1bcf24bf
FrancoLelli <francogastonlelli@gmail.com>
2023-05-14 12:26:36 -0300

    MR-25: Create put, delete and filter members

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import memberRouter from './member';
 
 const router = Router();
+
+router.use('/member', memberRouter);
 
 export default router;
---
+++ src/validations/member.js
@@ -0,0 +1,32 @@
+import Joi from 'joi';
+
+const validateUpdate = (req, res, next) => {
+  const updateMemberValidate = Joi.object({
+    firstName: Joi.string().min(3).max(20),
+    lastName: Joi.string().min(3).max(20),
+    dni: Joi.number().min(1000000).max(99999999).integer(),
+    phone: Joi.number().max(99999999).integer(),
+    email: Joi.string().email().min(5).max(30)
+      .lowercase(),
+    city: Joi.string().min(2).max(20),
+    birthDate: Joi.date(),
+    postalCode: Joi.number().max(9999).integer(),
+    isActive: Joi.boolean().default(false),
+    memberships: Joi.string().valid('Black', 'Classic', 'Only Classes').default('Classic'),
+  });
+
+  const validationUpdate = updateMemberValidate.validate(req.body);
+
+  if (!validationUpdate.error) return next();
+  return res.status(400).json({
+    message: `There was an error: ${validationUpdate.error.details[0].message}`,
+    data: undefined,
+    error: true,
+  });
+};
+
+const validationsMember = {
+  validateUpdate,
+};
+
+export default validationsMember;
