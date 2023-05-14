882d2631f2e399debb5f850faf6b720ed99799bf
Glenda Viera <glevide@gmail.com>
2023-05-14 20:39:57 -0300

    MR-21: Fixed updateValidation fields not required

--- src/validations/activity.js
+++ src/validations/activity.js
@@ -2,8 +2,8 @@ import Joi from 'joi';
 
 const validateUpdate = (req, res, next) => {
   const activityUpdateValidation = Joi.object({
-    name: Joi.string().min(3).max(20).required(),
-    description: Joi.string().min(10).max(120).required(),
+    name: Joi.string().min(3).max(20),
+    description: Joi.string().min(10).max(120),
   });
   const validation = activityUpdateValidation.validate(req.body);
   if (!validation.error) return next();
