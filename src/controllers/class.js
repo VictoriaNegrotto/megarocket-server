885c6760d789d66423d26f568bdb2ff752bc3610
Santiago <lacosantiago@outlook.com>
2023-05-14 18:38:06 -0300

    BaSP-MR-18: Created POST-GET classes

--- src/controllers/class.js
+++ src/controllers/class.js
@@ -102,8 +102,60 @@ const deleteClass = async (req, res) => {
   }
 };
 
+const createClass = async (req, res) => {
+  try {
+    const {
+      day, hour, trainer, activity, slots,
+    } = req.body;
+    const newClass = new Class({
+      day,
+      hour,
+      trainer,
+      activity,
+      slots,
+    });
+    const result = await newClass.save();
+
+    return res.status(201).json({
+      message: `Class with ID ${result.id} created!`,
+      data: result,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: error,
+      data: undefined,
+      error: true,
+    });
+  }
+};
+
+const getAllClasses = async (req, res) => {
+  try {
+    const classes = await Class.find({ isActive: true });
+    if (classes.length === 0) {
+      return res.status(404).json({
+        message: 'Classes not found',
+        data: undefined,
+        error: true,
+      });
+    }
+    return res.status(200).json({
+      message: 'Classes found!',
+      data: classes,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error,
+      data: undefined,
+      error: true,
+    });
+  }
+};
+
 const classController = {
-  updateClass, getClassById, deleteClass,
+  updateClass, getClassById, deleteClass, createClass, getAllClasses,
 };
 
 export default classController;
