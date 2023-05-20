ff4c38ba8863d843d3129d818b913850639d9337
Milagros <cerromilagros1@gmail.com>
2023-05-20 14:27:33 -0300

    MR-43: update unit test

--- src/tests/member.test.js
+++ src/tests/member.test.js
@@ -7,7 +7,7 @@ beforeAll(async () => {
   await Member.collection.insertMany(memberSeed);
 });
 
-describe('GET /api/member/:id', () => {
+describe('getMemberById /api/member/:id', () => {
   test('should return status 200 and member data when member exists', async () => {
     const response = await request(app).get('/api/member/646015ffa877f6e5fb0e5de3').send();
     expect(response.status).toBe(200);
@@ -17,6 +17,23 @@ describe('GET /api/member/:id', () => {
 
   test('should return status 404 when member does not exist', async () => {
     const response = await request(app).get('/api/member/646015ffa877f6e5fb0e5de5').send();
+    expect(response.status).toBe(404);
+    expect(response.body.error).toBe(true);
+    expect(response.body.data).toBeUndefined();
+  });
+});
+
+describe('updateMember /api/member/:id', () => {
+  test('should return status 200 and updated member data when member exists', async () => {
+    const updatedData = { firstName: 'Mili', lastName: 'Cerro' };
+    const response = await request(app).put('/api/member/646015ffa877f6e5fb0e5de3').send(updatedData);
+    expect(response.status).toBe(200);
+    expect(response.body.error).toBe(false);
+    expect(response.body.data).toBeDefined();
+  });
+  test('should return status 404 when member does not exist', async () => {
+    const updatedData = { firstName: 'Mili', lastName: 'Cerro' };
+    const response = await request(app).put('/api/member/646015ffa877f6e5fb0e5de5').send(updatedData);
     expect(response.status).toBe(404);
     expect(response.body.error).toBe(true);
     expect(response.body.data).toBeUndefined();
