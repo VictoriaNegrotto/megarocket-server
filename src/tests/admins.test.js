a246493cc0311caf350cef45875bd727976a3acd
Glenda Viera <glevide@gmail.com>
2023-05-20 19:53:20 -0300

    MR-38: added test for create admin request

--- src/tests/admins.test.js
+++ src/tests/admins.test.js
@@ -3,6 +3,16 @@ import app from '../app';
 import Admins from '../models/Admins';
 import adminSeed from '../seeds/admins';
 
+const mockAdmin = {
+  firstName: 'Manuel',
+  lastName: 'Cornet',
+  dni: 48632849,
+  phone: 34184609,
+  email: 'manu@gmail.com',
+  city: 'Rosario',
+  password: 'manu$test%admin',
+};
+
 beforeAll(async () => {
   await Admins.collection.insertMany(adminSeed);
 });
@@ -26,6 +36,22 @@ describe('getAdmins /api/admins', () => {
   test('should return a 500 status code when gets a database error', async () => {
     jest.spyOn(Admins, 'find').mockRejectedValueOnce(new Error('Database error'));
     const response = await request(app).get('/api/admins').send();
+    expect(response.status).toBe(500);
+    expect(response.body.error).toBeTruthy();
+  });
+});
+
+describe('createAdmin /api/admins', () => {
+  test('should return a 201 status code when admin is created', async () => {
+    const response = await request(app).post('/api/admins').send(mockAdmin);
+    expect(response.status).toBe(201);
+    expect(response.body.error).toBeFalsy();
+    expect(response.body.message).toBe(`Admin ${mockAdmin.firstName} was created successfully!`);
+  });
+
+  test('should return a 500 status code when gets a database error', async () => {
+    jest.spyOn(Admins, 'create').mockRejectedValueOnce(new Error('Database error'));
+    const response = await request(app).post('/api/admins').send(mockAdmin);
     expect(response.status).toBe(500);
     expect(response.body.error).toBeTruthy();
   });
