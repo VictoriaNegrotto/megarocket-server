dc79bf3f52280f7e9c37c6be476d1372e9b2ee66
lorenzomauro59 <lorenzomauro59@gmail.com>
2023-05-20 15:50:34 -0300

    MR-39 create delete test

--- src/tests/admins.test.js
+++ src/tests/admins.test.js
@@ -30,6 +30,8 @@ describe('updateAdmin /api/admins/:id', () => {
     const updateData = { firstName: 'Lorenzo', lastName: 'Mauro' };
     const response = await request(app).put('/api/admins/6462b798eb92c75e0c1040f1').send(updateData);
     expect(response.status).toBe(404);
+    expect(response.body.data).toBeUndefined();
+    expect(response.error).toBeTruthy();
   });
 
   test('should return status 500', async () => {
@@ -37,6 +39,29 @@ describe('updateAdmin /api/admins/:id', () => {
     const updateData = { firstName: 'Lorenzo' };
     const response = await request(app).put('/api/admins/6462b798eb92c75e0c1040f0').send(updateData);
     expect(response.status).toBe(500);
-    expect(response.body.error).toBeTruthy();
+    expect(response.body.data).toBeUndefined();
+    expect(response.error).toBeTruthy();
+  });
+});
+
+describe('deleteAdmin /api/admins/:id', () => {
+  test('should delete one admin', async () => {
+    const response = await request(app).delete('/api/admins/6462b798eb92c75e0c1040f7').send();
+    expect(response.status).toBe(200);
+  });
+
+  test('should return status 404', async () => {
+    const response = await request(app).delete('/api/admins/6462b798eb92c75e0c1040f2').send();
+    expect(response.status).toBe(404);
+    expect(response.body.data).toBeUndefined();
+    expect(response.error).toBeTruthy();
+  });
+
+  test('should return status 500', async () => {
+    jest.spyOn(Admin, 'findOne').mockRejectedValueOnce(new Error('Database error'));
+    const response = await request(app).delete('/api/admins/6462b798eb92c75e0c1040f7').send();
+    expect(response.status).toBe(500);
+    expect(response.body.data).toBeUndefined();
+    expect(response.error).toBeTruthy();
   });
 });
