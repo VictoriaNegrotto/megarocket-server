d83283a7785302b80d949d99de6dea4aa0bdbb9c
Juan Cruz Tilatti <jctilatti@gmail.com>
2023-05-21 16:30:26 -0300

    MR-44: finished my first validations

---
+++ src/seeds/subscription.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+export default [
+  {
+    _id: new mongoose.Types.ObjectId('646276eb6d374da6dcbc5de5'),
+    class: '646004aff33f9c83d28ed968',
+    members: ['646004aff33f9c83d28ed958', '64601a15f33f9c83d28ed968'],
+    date: '2023-06-15T14:57:14.000+00:00',
+    isActive: true,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('646276eb6d374da6dcbc5de6'),
+    class: '646004aff33f9c83d28ed969',
+    members: ['646004aff33f9c83d28ed959'],
+    date: '2023-06-15T14:57:14.000+00:00',
+    isActive: true,
+  },
+];
--- src/tests/subscription.test.js
+++ src/tests/subscription.test.js
@@ -1,5 +1,41 @@
-// Basic test to avoid conflicts
-// REMOVE THIS TEST AND MAKE YOURS!
-test('should pass a basic test', () => {
-  expect(1 + 1).toBe(2);
+import request from 'supertest';
+import app from '../app';
+import Subscription from '../models/Subscription';
+import subscriptionSeed from '../seeds/subscription';
+
+const mockSubscription = {
+  classes: '646004aef33f9c83d28ed950',
+  members: ['646004aff33f9c83d28ed950'],
+  date: '2023-06-15T14:57:14.000+00:00',
+};
+
+beforeAll(async () => {
+  await Subscription.collection.insertMany(subscriptionSeed);
+});
+
+describe('getAllSubscriptions /api/subscriptions', () => {
+  test('should return status 200', async () => {
+    const response = await request(app).get('/api/subscriptions').send();
+    expect(response.status).toBe(200);
+    expect(response.error).toBeFalsy();
+  });
+
+  test('should return status 404', async () => {
+    const response = await request(app).get('/api/subscription').send();
+    expect(response.status).toBe(404);
+    expect(response.error).toBeTruthy();
+  });
+});
+
+describe('createSubscription /api/subscriptions', () => {
+  test('should create a subscription and return status 201', async () => {
+    const response = await request(app).post('/api/subscriptions').send(mockSubscription);
+    expect(response.status).toBe(201);
+    expect(response.error).toBeFalsy();
+  });
+
+  test('should return status 404', async () => {
+    const response = await request(app).post('/api/subscription').send(mockSubscription);
+    expect(response.status).toBe(404);
+  });
 });
