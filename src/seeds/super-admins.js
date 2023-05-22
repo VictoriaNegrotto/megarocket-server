bf26554df06e3200db7caf6fb6ad3d875d39d61a
Alejandro Schmidt <alemusho@gmail.com>
2023-05-21 22:45:19 -0300

    MR-34: super admin seeder done

---
+++ src/seeds/super-admins.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+export default [
+  {
+    _id: new mongoose.Types.ObjectId('646154d30aa90c4527db6f09'),
+    email: 'ariimaldo@gmail.com',
+    password: 'mauro123',
+    isActive: true,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6461554c0aa90c4527db6f0e'),
+    email: 'juvi@gmail.com',
+    password: 'juvi1234',
+    isActive: true,
+
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6462bdaaeb92c75e0c104108'),
+    email: 'glevi@gmail.com',
+    password: 'delgado123',
+    isActive: true,
+    __v: 0,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6461553b0aa90c4527db6f0c'),
+    email: 'francoa@gmail.com',
+    password: 'franco123',
+    isActive: false,
+  },
+];
