ce79bcf62bab9afcc298b8e65345a05e2f6615f6
Gonzarey96 <gonzareybet@gmail.com>
2023-05-22 14:59:57 -0300

    MR-45: added update test

---
+++ src/seeds/subscription.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+export default [
+  {
+    _id: new mongoose.Types.ObjectId('646276eb6d374da6dcbc5de5'),
+    classes: new mongoose.Types.ObjectId('6462c6beeca7042f29eb1164'),
+    members: [new mongoose.Types.ObjectId('646004aff33f9c83d28ed958')],
+    date: '2023-06-15T14:57:14.000Z',
+    isActive: true,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('646276eb6d374da6dcbc5de7'),
+    classes: new mongoose.Types.ObjectId('6462c6beeca7042f29eb1164'),
+    members: [new mongoose.Types.ObjectId('646004aff33f9c83d28ed958')],
+    date: '2023-06-15T14:57:14.000Z',
+    isActive: true,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('646276eb6d374da6dcbc5de6'),
+    classes: new mongoose.Types.ObjectId('6462c6beeca7042f29eb1164'),
+    members: [new mongoose.Types.ObjectId('646004aff33f9c83d28ed958')],
+    date: '2023-06-15T14:57:14.000Z',
+    isActive: false,
+  },
+];
