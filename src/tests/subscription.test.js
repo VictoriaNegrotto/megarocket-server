e8fc8d02043e2b04795a74087a2f379359238c71
Juan Cruz Tilatti <jctilatti@gmail.com>
2023-05-22 00:01:10 -0300

    MR-44: finished post test

--- src/tests/subscription.test.js
+++ src/tests/subscription.test.js
@@ -9,6 +9,11 @@ const mockSubscription = {
   date: '2023-06-15T14:57:14.000+00:00',
 };
 
+const failedMockSubscription = {
+  classes: '646004aef33f9c83d28ed950',
+  date: '2023-06-15T14:57:14.000+00:00',
+};
+
 beforeAll(async () => {
   await Subscription.collection.insertMany(subscriptionSeed);
 });
@@ -44,11 +49,33 @@ describe('createSubscription /api/subscriptions', () => {
   test('should create a subscription and return status 201', async () => {
     const response = await request(app).post('/api/subscriptions').send(mockSubscription);
     expect(response.status).toBe(201);
+    expect(response.body.message).toBe('Subscription was created successfully!');
     expect(response.error).toBeFalsy();
+    expect(response.body.data).toBeDefined();
   });
 
   test('should return status 404', async () => {
     const response = await request(app).post('/api/subscription').send(mockSubscription);
     expect(response.status).toBe(404);
+    expect(response.body.message).toBeUndefined();
+    expect(response.error).toBeTruthy();
+    expect(response.body.data).toBeUndefined();
+  });
+
+  test('should return status 400', async () => {
+    const response = await request(app).post('/api/subscriptions').send(failedMockSubscription);
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBeDefined();
+    expect(response.error).toBeTruthy();
+    expect(response.body.data).toBeUndefined();
+  });
+
+  test('should return status 500', async () => {
+    jest.spyOn(Subscription, 'create').mockImplementation(new Error('Error'));
+    const response = await request(app).post('/api/subscriptions').send(mockSubscription);
+    expect(response.status).toBe(500);
+    expect(response.error.message).toEqual('cannot POST /api/subscriptions (500)');
+    expect(response.error).toBeTruthy();
+    expect(response.body.data).toBeUndefined();
   });
 });
