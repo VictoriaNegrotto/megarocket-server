eb65142bb6157454f075b0e79bd326d3bb516c6e
FrancoLelli <francogastonlelli@gmail.com>
2023-05-22 14:31:18 -0300

    MR-41: Create test in Get By Id when status is 200 in Class

--- src/tests/class.test.js
+++ src/tests/class.test.js
@@ -1,5 +1,19 @@
-// Basic test to avoid conflicts
-// REMOVE THIS TEST AND MAKE YOURS!
-test('should pass a basic test', () => {
-  expect(1 + 1).toBe(2);
+import request from 'supertest';
+import app from '../app';
+import Class from '../models/Class';
+import classSeed from '../seeds/class';
+
+beforeAll(async () => {
+  await Class.collection.insertMany(classSeed);
+});
+
+describe('getClassById /api/class/:id', () => {
+  test('should return status 200 when id exists', async () => {
+    const id = '646004aff33f9c83d28ed954';
+    const response = await request(app).get(`/api/class/${id}`).send();
+    expect(response.status).toBe(200);
+    expect(response.body.error).toBe(false);
+    expect(response.body.data).toBeDefined();
+    expect(response.body.message).toContain(`Class with ID ${id} was found!`);
+  });
 });
