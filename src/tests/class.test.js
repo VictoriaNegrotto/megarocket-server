06ca4ade4ab6a684071b76c3bcf6f04aa7992bb0
Walter Ariel Pavicich <walterarielpavicich@gmail.com>
2023-05-22 22:21:37 -0300

    MR-40: get all classes status 500 test added

--- src/tests/class.test.js
+++ src/tests/class.test.js
@@ -32,4 +32,12 @@ describe('getAllClass /api/class', () => {
     expect(response.body.data).toBeUndefined();
     await Class.collection.insertMany(classSeed);
   });
+  test('should be return status 500 when database error has ocurred', async () => {
+    jest.spyOn(Class, 'find').mockImplementation(() => { throw new Error('Database error'); });
+    const response = await request(app).get('/api/class').send();
+    expect(response.status).toBe(500);
+    expect(response.error.message).toBe('cannot GET /api/admins (500)');
+    expect(response.body.error).toBeTruthy();
+    expect(response.body.data).toBeUndefined();
+  });
 });
