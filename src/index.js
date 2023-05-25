aa1e67aed6116ac76f7486cb13bae989b112e9e3
julianv97 <julian.vicente@radiumrocket.com>
2023-05-25 18:18:06 -0300

    MR: update index

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_DB_CONNECT_URL, { maxPoolSize: process.env.MONGO_POOLSIZE || 1 })
+mongoose.connect(process.env.DB_URL, { maxPoolSize: process.env.MONGO_POOLSIZE || 1 })
   .then(() => console.log('🟢 DB Connected!'))
   .then(() => app.listen(PORT, () => console.log(`🚀 Server started on port ${PORT}!`)))
   .catch((error) => console.log('🔴 There was an error on the DB connection method.', error));
