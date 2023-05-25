c5867d5d604b1b868cdae7ddbaf7b4ab43d7da66
julianv97 <julian.vicente@radiumrocket.com>
2023-05-25 18:08:45 -0300

    MR: update index

--- src/index.js
+++ src/index.js
@@ -4,23 +4,19 @@ import app from './app';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_DB_CONNECT_URL)
+mongoose.connect(process.env.MONGO_DB_CONNECT_URL, { maxPoolSize: process.env.MONGO_POOLSIZE || 1 })
 // eslint-disable-next-line no-console
   .then(() => console.log('MongoDB connected'))
+  .then(() => {
+    app.listen(process.env.PORT, (err) => {
+      if (err) throw err;
+      // eslint-disable-next-line no-console
+      console.log(`server listening on port: ${process.env.PORT}`);
+    });
+  })
 // eslint-disable-next-line no-console
   .catch((e) => console.log(e));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
-try {
-  app.listen(process.env.PORT, (err) => {
-    if (err) throw err;
-    // eslint-disable-next-line no-console
-    console.log(`server listening on port: ${process.env.PORT}`);
-  });
-} catch (err) {
-  // eslint-disable-next-line no-console
-  console.log('There was an error starting the server: ', err);
-}
