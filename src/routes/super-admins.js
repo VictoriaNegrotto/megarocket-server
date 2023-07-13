9bd723012e28bbfbdcf31ef62e3fde135eab56de
moradpablo <moradpabloivan@gmail.com>
2023-07-13 20:27:07 -0300

    MR-196: solved problem with superadmin routes

--- src/routes/super-admins.js
+++ src/routes/super-admins.js
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 import validations from '../validations/super-admins';
 import superAdminController from '../controllers/super-admins';
+import verifyToken from '../middlewares/authMiddleware';
 
 const superAdminRoute = Router();
 
 superAdminRoute
-  .get('/', superAdminController.getAllSuperAdmin)
-  .get('/:id', superAdminController.getSuperAdminById)
-  .get('/filter/:emailFilter', superAdminController.getSuperAdminByEmail)
-  .post('/', validations.validateCreate, superAdminController.createSuperAdmin)
-  .put('/:id', validations.validateUpdate, superAdminController.updateSuperAdmin)
-  .delete('/:id', superAdminController.deleteSuperAdmin);
+  .get('/', verifyToken(['SUPERADMIN']), superAdminController.getAllSuperAdmin)
+  .get('/:id', verifyToken(['SUPERADMIN']), superAdminController.getSuperAdminById)
+  .get('/filter/:emailFilter', verifyToken(['SUPERADMIN']), superAdminController.getSuperAdminByEmail)
+  .post('/', verifyToken(['SUPERADMIN']), validations.validateCreate, superAdminController.createSuperAdmin)
+  .put('/:id', verifyToken(['SUPERADMIN']), validations.validateUpdate, superAdminController.updateSuperAdmin)
+  .delete('/:id', verifyToken(['SUPERADMIN']), superAdminController.deleteSuperAdmin);
 
 export default superAdminRoute;
